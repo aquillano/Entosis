56eec86421a8ba28964a85ac1d615b0fe4a34566
Steve <steveaquillano@gmail.com>
2010-10-16 00:53:44 -0400

    added acceleration/decceleration to player mote

--- entosis/Entosis.java
+++ entosis/Entosis.java
@@ -10,7 +10,9 @@ public class Entosis extends PApplet {
 	
 	private int screenX = 480;
 	private int screenY = 320;
-	private int backgroundColor = color(64,64,64);
+	private int backgroundColor = color(32,32,32);
+	private int clickCounter = 0;
+	private PVector vels[] = new PVector[10];
 	
 	Player player;
 	ArrayList<Mote> moteList = new ArrayList<Mote>();
@@ -26,6 +28,10 @@ public class Entosis extends PApplet {
 		
 		for( int i = 0; i < 5; i++ ) {
 			moteList.add( new Mote(random(5,15)) );
+		}
+		
+		for ( int i=0; i<10; i++ ) {
+			vels[i] = new PVector(0,0);
 		}
 		
 	}
@@ -45,7 +51,18 @@ public class Entosis extends PApplet {
 	}
 	
 	public void mouseClicked() {
-		player.velocity = new PVector(player.position.x - mouseX, player.position.y - mouseY);
-		player.velocity.normalize();
+		clickCounter++;
+		PVector sum = new PVector(0,0);
+		
+		vels[clickCounter%10] = new PVector(player.position.x - mouseX, player.position.y - mouseY);
+		vels[clickCounter%10].normalize();
+		
+		for ( int i=0; i<10; i++ ) {
+			sum.add(vels[i]);
+		}
+		
+		sum.div(10); // Get average
+		player.velocity = sum;
 	}
+	
 }
